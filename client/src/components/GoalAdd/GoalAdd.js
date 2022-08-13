745f3319fa6ed0910efd578db4ac450dfd09b493
Hermanda Mak <hermandamak@gmail.com>
2022-08-12 20:56:10 -0700

    create form for goals

--- client/src/components/GoalAdd/GoalAdd.js
+++ client/src/components/GoalAdd/GoalAdd.js
@@ -1,6 +1,33 @@
-const GoalAdd = () => {
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import GoalList from '../GoalList/GoalList';
+
+
+const GoalAdd = (props) => {
+
+  const {category} = useParams();
+  const {userId} = props;
+  const navigate = useNavigate();
+
+  const [goal, setGoal] = useState({
+    "description": "",
+    "completeBy": "",
+  });
+  const [error, setError] = useState({});
+
+  const postSubmit = (successResponse) => {
+    axios.post(`http://localhost:5000/api/${category}`, {...goal, createdBy: userId})
+    .then(res => navigate('/dashboard'))
+    .catch(err => {
+      setError(err.response.data)
+      console.log(err)
+    })
+  }
+
   return (
-    <div>GoalAdd</div>
+    <GoalList action={"Add"} category={"nutrition"} userId={userId} submitAction={postSubmit} goal={goal} setGoal={setGoal} error={error} />
   )
 }
 export default GoalAdd
--- client/src/components/GoalUpdate/GoalUpdate.js
+++ client/src/components/GoalUpdate/GoalUpdate.js
@@ -1,6 +1,47 @@
-const GoalUpdate = () => {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import GoalList from '../GoalList/GoalList';
+
+
+const GoalUpdate = (props) => {
+
+  const {userId} = props;
+  const {id, category} = useParams;
+  const navigate = useNavigate();
+
+  const [goal, setGoal] = useState({
+    "description": "",
+    "completeBy": "",
+  });
+  const [error, setError] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // axios.get(`http://localhost:5000/api/posts/${id}`)
+    // .then( res => {
+    //   setGoal(res.data)
+      setLoaded(true)
+    // })
+    // .catch(err => console.log(err))
+  }, [])
+
+  const postSubmit = (successResponse) => {
+    axios.put(`http://localhost:5000/api/${category}`, {...goal, createdBy: userId})
+    .then(res => navigate('/dashboard'))
+    .catch(err => {
+      setError(err.response.data)
+      console.log(err)
+    })
+  }
+
   return (
-    <div>GoalUpdate</div>
+    <>
+    { loaded &&
+      <GoalList action={"Update"} category={"nutrition"} userId={userId} submitAction={postSubmit} goal={goal} setGoal={setGoal} error={error} />
+    }
+    </>
   )
 }
 export default GoalUpdate
