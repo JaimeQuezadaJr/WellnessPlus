ee34c9ee94a56f4324b0a44011665ba392c785da
Hermanda Mak <hermandamak@gmail.com>
2022-08-12 18:07:32 -0700

    add nav links and styling

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,31 @@
-const Header = () => {
+import {useNavigate, Link} from 'react-router-dom';
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const Header = (props) => {
+
+  const navigate = useNavigate();
+
+
   return (
-    <div>Header</div>
+    <div>
+      <Navbar expand="md" className='pt-3'>
+        <Container>
+          <Navbar.Brand as={Link} to={'/'}>WellnessPlus</Navbar.Brand>
+          <div className='justify-content-end'>
+            <Navbar.Toggle aria-controls='wellnessplus-nav' />
+            <Navbar.Collapse id="wellnessplus-nav">
+              <Nav className='me-auto'>
+                <Nav.Link as={Link} to={'/about'}>About</Nav.Link>
+                <Nav.Link as={Link} to={'/register'}>Register</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        </Container>
+      </Navbar>
+    </div>
   )
 }
 export default Header;
