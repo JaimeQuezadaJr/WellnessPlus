7082bf919844d8b653aa6a83c5eeb5e2eb0c084f
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-08-15 10:59:28 -0700

    added nutrition model

--- server/models/nutrition.model.js
+++ server/models/nutrition.model.js
@@ -4,3 +4,28 @@
 //   type: mongoose.Schema.Types.ObjectId,
 //   ref: 'User'
 // }
+const mongoose = require('mongoose');
+
+const NutritionSchema = mongoose.Schema(
+    {
+        description: {
+            type: String,
+            required: [true, 'Goal is required'],
+            minLength: [12, 'Goal description should be more than 12 characters long']
+        },
+        completedBy: {
+            type: Date,
+            required: [true, 'Please add goal completion date']
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Nutrition = mongoose.model('nutrition', NutritionSchema);
+module.exports = Nutrition;
