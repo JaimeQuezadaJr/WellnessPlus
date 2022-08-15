c8fa068d14106b57937fe9fe158e19e642c60440
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-08-15 10:42:33 -0700

    added login and registration methods

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,72 @@
 const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.JWT_SECRET;
+console.log('SECRET', SECRET);
+
+module.exports.register = (req, res) => {
+    try {
+        const user = new User(req.body);
+        const newUser = await user.save();
+        console.log('USER CREATED', newUser);
+        const userToken = jwt.sign(
+            { _id: newUser._id, email: newUser.email, username: newUser.username },
+            SECRET,
+        );
+        console.log('JWT:', userToken);
+        res
+            .status(201)
+            .cookie('userToken', userToken, { expires: new Date(Date.now() + 900000) })
+            .json({ successMessage: 'user created', user: newUser });
+        } catch (error) {
+        console.log('Register ERROR', error);
+        res.status(400).json(error);
+    }
+}
+
+module.exports.login = (req, res) => {
+    const userDocument = await User.findOne({ email: req.body.email });
+    console.log('USERDOC', userDocument);
+    if (!userDocument) {
+        res.status(400).json({ error: 'invalid email/password' });
+    } else {
+        try {
+        const isPasswordValid = await bcrypt.compare(req.body.password, userDocument.password);
+        if (!isPasswordValid) {
+            res.status(400).json({ error: 'invalid email/password' });
+        } else {
+        const userToken = jwt.sign(
+            { _id: userDocument._id, email: userDocument.email, username: userDocument.username },
+            SECRET,
+        );
+        console.log('JWT:', userToken);
+        res
+            .status(201)
+            .cookie('userToken', userToken, { expires: new Date(Date.now() + 900000) })
+            .json({ successMessage: 'user loggedin', user: userDocument });
+        }
+    } catch (error) {
+        console.log('LOGIN ERROR', error);
+        res.status(400).json({ error: 'invalid email/password' });
+        }
+    }
+}
+
+module.exports.logout = (req, res) => {
+    res.clearCookie('userToken');
+    res.json({ successMessage: 'User logged out' });
+}
+
+module.exports.getLoggedInUser = (req, res) => {
+    const user = jwt.verify(req.cookies.userToken, SECRET);
+    User.findOne({ _id: user._id })
+        .then((user) => {
+            res.json(user);
+    })
+        .catch((err) => {
+            console.log(err);
+    });
+}
 
 module.exports.findAllUsers = (req, res) => {
     User.find()
