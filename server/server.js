80a98cb869b3aece5f59f08385235f4461b1c69f
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-08-15 11:08:37 -0700

    server updates

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
+require ('./config/mongoose.config')
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use(express.json());
 
 app.use(cors({ origin: 'http://localhost:3000'}));
 // app.use(cors());
 
-require ('./config/mongoose.config')
 
-app.use(express.json());
-
-const routes = require('./routes/user.routes');
-routes(app);
+const userRoutes = require('./routes/user.routes');
+userRoutes(app);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
