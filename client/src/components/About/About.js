2496e37f873ffcfc8242e6571c46b29ca3b28bb6
Hermanda Mak <hermandamak@gmail.com>
2022-08-14 19:34:12 -0700

    add styling and text placeholders

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,6 +1,20 @@
-const About = () => {
+import { useNavigate } from 'react-router-dom';
+
+import Button from 'react-bootstrap/Button';
+import styles from './About.module.css';
+
+const About = (props) => {
+
+  const navigate = useNavigate();
+
   return (
-    <div>About</div>
+    <div className={"d-flex justify-content-center p-5"}>
+      <div className={styles.aboutText}>
+        <h3 className={styles.textContainer}>Become more in tune with yourself!</h3>
+        <p className={styles.textContainer}>Commit yourself to achieving your maximum wellness goals!</p>
+        <Button variant={"outline-dark"} onClick={() => navigate('/')}>Get Started</Button>
+      </div>
+    </div>
   )
 }
 export default About
