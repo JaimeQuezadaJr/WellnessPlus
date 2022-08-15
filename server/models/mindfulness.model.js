f0113b3e5e9701aec871bafbf29ba172c94cad2e
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-08-15 11:01:53 -0700

    added mindfulness model

--- server/models/mindfulness.model.js
+++ server/models/mindfulness.model.js
@@ -4,3 +4,29 @@
 //   type: mongoose.Schema.Types.ObjectId,
 //   ref: 'User'
 // }
+
+const mongoose = require('mongoose');
+
+const MindfulnessSchema = mongoose.Schema(
+    {
+        description: {
+            type: String,
+            required: [true, 'Goal is required'],
+            minLength: [12, 'Goal description should be more than 12 characters long']
+        },
+        completedBy: {
+            type: Date,
+            required: [true, 'Please add goal completion date']
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Mindfulness = mongoose.model('mindfulness', MindfulnessSchema);
+module.exports = Mindfulness;
