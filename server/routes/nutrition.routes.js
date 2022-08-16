93cfb739bf0d46471835e7a93ed0fbcf3d859889
Hermanda Mak <hermandamak@gmail.com>
2022-08-15 17:42:41 -0700

    set up API routes

---
+++ server/routes/nutrition.routes.js
@@ -0,0 +1,18 @@
+const NutritionController = require('../controllers/nutrition.controller');
+// const { authenticate } = require('../config/jwt.config'); //TODO include if implement JWT
+
+module.exports = (app) => { //app = express()
+  app.get('/api/nutrition/user/:userId', NutritionController.findNutritionByUser);
+  app.get('/api/nutrition/:id', NutritionController.findOneNutrition);
+  app.post('/api/nutrition', NutritionController.createNewNutrition);
+  app.put('/api/nutrition/:id', NutritionController.updateNutrition);
+  app.delete('/api/nutrition/:id', NutritionController.deleteNutrition);
+  
+  //TODO use below if implement JWT
+  // app.get('/api/nutrition/user/:userId', authenticate, NutritionController.findNutritionByUser);
+  // app.get('/api/nutrition/:id', authenticate, NutritionController.findOneNutrition);
+  // app.post('/api/nutrition', authenticate, NutritionController.createNewNutrition);
+  // app.put('/api/nutrition/:id', authenticate, NutritionController.updateNutrition);
+  // app.delete('/api/nutrition/:id', authenticate, NutritionController.deleteNutrition);
+
+}
