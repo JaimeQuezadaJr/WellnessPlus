138c5dff2be416c382e525ae3a65acf0148abbe0
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-08-16 13:39:06 -0700

    added user state

--- client/src/components/UserRegistration/UserRegistration.js
+++ client/src/components/UserRegistration/UserRegistration.js
@@ -1,6 +1,21 @@
-const UserRegistration = () => {
+import { useState } from 'react'
+import axios from 'axios' 
+import { useNavigate } from 'react-router-dom'
+
+const UserRegistration = ({ setIsLoggedin }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    age:0,
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  
   return (
     <div>UserRegistration</div>
   )
 }
+
 export default UserRegistration;
