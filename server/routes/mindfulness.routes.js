01e354b771b9683c44ebd8ec500c34abbaa3250f
Hermanda Mak <hermandamak@gmail.com>
2022-08-16 22:24:46 -0700

    correct controller methods name

--- server/routes/mindfulness.routes.js
+++ server/routes/mindfulness.routes.js
@@ -2,17 +2,18 @@ const MindfulnessController = require('../controllers/mindfulness.controller');
 // const { authenticate } = require('../config/jwt.config'); //TODO include if implement JWT
 
 module.exports = (app) => { //app = express()
-  app.get('/api/mindfulness/user/:userId', MindfulnessController.findNutritionByUser);
-  app.get('/api/mindfulness/:id', MindfulnessController.findOneNutrition);
-  app.post('/api/mindfulness', MindfulnessController.createNewNutrition);
-  app.put('/api/mindfulness/:id', MindfulnessController.updateNutrition);
-  app.delete('/api/mindfulness/:id', MindfulnessController.deleteNutrition);
+  app.get('/api/mindfulness', MindfulnessController.findAllMindfulness);
+  app.get('/api/mindfulness/user/:userId', MindfulnessController.findMindfulnessByUser);
+  app.get('/api/mindfulness/:id', MindfulnessController.findOneMindfulness);
+  app.post('/api/mindfulness', MindfulnessController.createMindfulness);
+  app.put('/api/mindfulness/:id', MindfulnessController.updateMindfulness);
+  app.delete('/api/mindfulness/:id', MindfulnessController.deleteMindfulness);
   
   //TODO use below if implement JWT
-  // app.get('/api/mindfulness/user/:userId', authenticate, MindfulnessController.findNutritionByUser);
-  // app.get('/api/mindfulness/:id', authenticate, MindfulnessController.findOneNutrition);
-  // app.post('/api/mindfulness', authenticate, MindfulnessController.createNewNutrition);
-  // app.put('/api/mindfulness/:id', authenticate, MindfulnessController.updateNutrition);
-  // app.delete('/api/mindfulness/:id', authenticate, MindfulnessController.deleteNutrition);
+  // app.get('/api/mindfulness/user/:userId', authenticate, MindfulnessController.findMindfulnessByUser);
+  // app.get('/api/mindfulness/:id', authenticate, MindfulnessController.findOneMindfulness);
+  // app.post('/api/mindfulness', authenticate, MindfulnessController.createNewMindfulness);
+  // app.put('/api/mindfulness/:id', authenticate, MindfulnessController.updateMindfulness);
+  // app.delete('/api/mindfulness/:id', authenticate, MindfulnessController.deleteMindfulness);
 
 }
