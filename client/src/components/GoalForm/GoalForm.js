ebc8fbae483135fb22f90c1a993aecee007e1968
Hermanda Mak <hermandamak@gmail.com>
2022-08-17 00:20:49 -0700

    add date parser and errors display

--- client/src/components/GoalForm/GoalForm.js
+++ client/src/components/GoalForm/GoalForm.js
@@ -5,7 +5,12 @@ const GoalForm = (props) => {
 
   const {action, category, submitAction, goal, setGoal, error} = props;
 
-
+  const datePickerParse = (date) => {
+    let dateObj = new Date(date);
+    let day = `0${dateObj.getDate()}`.slice(-2);
+    let month = `0${dateObj.getMonth() + 1}`.slice(-2);
+    return `${dateObj.getFullYear()}-${month}-${day}`;
+  }
 
   const onChangeHandler = (e) => {
     setGoal({...goal, [e.target.name]: e.target.value });
@@ -16,23 +21,22 @@ const GoalForm = (props) => {
     submitAction();
   }
 
-
   return (
     <div className='d-flex flex-column'>
-      <h3>{action} {category} Goal</h3>
-      <Form onSubmit={onSubmitHandler}>
+      <h3>{action} {category.toUpperCase()} Goal</h3>
+      <Form onSubmit={ onSubmitHandler }>
         <Form.Group controlId='description'>
           <Form.Label>Description</Form.Label>
           <Form.Control type='textarea' row={3} name='description' onChange={onChangeHandler} value={goal.description}/>
-          {error.description &&
-            <Form.Text className='text-danger'>{error.description}</Form.Text>
+          {error.description && 
+            <Form.Text className='text-danger'>{error.description.message}</Form.Text>
           }
         </Form.Group>
-        <Form.Group controlId='completeBy'>
-          <Form.Label>Complete By</Form.Label>
-          <Form.Control type='date' name='completeBy' onChange={onChangeHandler} value={goal.completeBy}/>
-          {error.completeBy &&
-            <Form.Text className='text-danger'>{error.completeBy}</Form.Text>
+        <Form.Group controlId='completedBy'>
+          <Form.Label>Completed By</Form.Label>
+          <Form.Control type='date' name='completedBy' onChange={onChangeHandler} value={datePickerParse(goal.completedBy)}/>
+          {error.completedBy && 
+            <Form.Text className='text-danger'>{error.completedBy.message}</Form.Text>
           }
         </Form.Group>
         <div className='d-grid d-md-block'>
