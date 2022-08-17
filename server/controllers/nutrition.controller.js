00e73ff37fbb56efed1d161c9078013899c1fcd3
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-08-16 19:30:22 -0700

    fixed controllers

--- server/controllers/nutrition.controller.js
+++ server/controllers/nutrition.controller.js
@@ -5,11 +5,13 @@ const User = require('../models/user.model');
 
 
 module.exports.findAllNutritions = (req, res) => {
-    Nutrition.find()
+    Nutrition.find({})
+        .populate('createdBy', 'email')
         .then((allNutritions) => {
             res.json(allNutritions)
         })
         .catch((err) => {
+            console.log('ERROR IN Get all', err);
             res.status(400).json({message: 'Something went wrong', error:err})
         });
 }
@@ -20,32 +22,37 @@ module.exports.findOneNutrition = (req, res) => {
             res.json(oneNutrition)
         })
         .catch((err) => {
+            console.log('ERROR IN Get Nutrition', err);
             res.status(400).json({message: 'Something went wrong', error:err})
         });
 }
 
 module.exports.findNutritionByUser = (req, res) => {
-  // User.findOne({ username: req.params.username }).then((user) => {   //TODO decide whether to keep it until complete front end useState/localstorage
-  //   console.log('USERID', user._id);
-    Nutrition.find({ createdBy: req.params.userId }) //TODO may need to change find parameter (user._id)
-      .populate('createdBy', '_id email')
-      .then((nutrition) => {
-        res.json(nutrition);
-      })
-      .catch((err) => {
-        res.status(400).json({ message: 'something went wrong in find all nutrition', error: err });
-      })
+    console.log('IS THIS WORKING', req.params.email);
+    User.findOne({ email: req.params.email }).then((user) => {   //TODO decide whether to keep it until complete front end useState/localstorage
+        console.log('USERID', user._id);
+        Nutrition.find({ createdBy: req.params.userId }) //TODO may need to change find parameter (user._id)
+        .populate('createdBy', '_id email')
+        .then((nutrition) => {
+            res.json(nutrition);
+        })
+        .catch((err) => {
+            console.log('ERROR', err);
+            res.status(400).json({ message: 'something went wrong in find all nutrition', error: err });
+        })
+})
 }
 
 module.exports.createNutrition = (req, res) => {
     //TODO may needed when implementing JWT
-    // const user = jwt.verify(req.cookies.userToken, SECRET);
-    // Nutrition.create({ ...req.body, createdBy: user._id })
-    Nutrition.create(req.body)
+    const user = jwt.verify(req.cookies.userToken, SECRET);
+    Nutrition.create({ ...req.body, createdBy: user._id })
+    // Nutrition.create(req.body)
         .then((newNutrition) => {
             res.status(201).json(newNutrition)
         })
         .catch((err) => {
+            console.log('ERROR IN create nutrition', err);
             res.status(400).json({message: 'Something went wrong', error:err})
         });
 }
@@ -59,6 +66,7 @@ module.exports.updateNutrition = (req, res) => {
             res.json(updateNutrition)
         })
         .catch((err) => {
+            console.log('ERROR IN update nutrition', err);
             res.status(400).json({message: 'Something went wrong', error:err})
         });
 
