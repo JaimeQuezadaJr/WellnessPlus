793ea699587fc5e93194b4a411ed38533ae11d3d
Hermanda Mak <hermandamak@gmail.com>
2022-08-17 14:31:33 -0700

    remove jwt contents

--- server/controllers/fitness.controller.js
+++ server/controllers/fitness.controller.js
@@ -3,75 +3,63 @@ const jwt = require('jsonwebtoken');
 const SECRET = process.env.JWT_SECRET;
 const User = require('../models/user.model');
 
+
 module.exports.findAllFitnessGoals = (req, res) => {
-    Nutrition.find({})
-        .populate('createdBy', 'email')
-        .then((allFitnessGoals) => {
-            res.json(allFitnessGoals)
-        })
-        .catch((err) => {
-            console.log('ERROR IN Get all', err);
-            res.status(400).json({message: 'Something went wrong', error:err})
-        });
+  Fitness.find()
+    .then((allFitness) => {
+        res.json(allFitness)
+    })
+    .catch((err) => {
+        res.status(400).json({message: 'Something went wrong', error:err})
+    });
 }
 
-module.exports.findOneFitness = (req, res) => {
-    Fitness.findOne({_id:req.params.id})
-        .then((oneFitness) => {
-            res.json(oneFitness)
-        })
-        .catch((err) => {
-            console.log('ERROR IN Get Fitness', err);
-            res.status(400).json({message: 'Something went wrong', error:err})
-        });
+module.exports.findFitness = (req, res) => {
+  Fitness.findOne({_id:req.params.id})
+    .then((fitness) => {
+        res.json(fitness)
+    })
+    .catch((err) => {
+        res.status(400).json({message: 'Something went wrong', error:err})
+    });
 }
 
 module.exports.findFitnessByUser = (req, res) => {
-  // console.log('IS THIS WORKING', req.params.email);
-  User.findOne({ email: req.params.email })
-    .then((user) => {   //TODO decide whether to keep it until complete front end useState/localstorage
-      // console.log('USERID', user._id);
-      Fitness.find({ createdBy: req.params.userId }) //TODO may need to change find parameter (user._id)
+  // User.findOne({ username: req.params.username }).then((user) => {   //TODO decide whether to keep it until complete front end useState/localstorage
+  //   console.log('USERID', user._id);
+    Fitness.find({ createdBy: req.params.userId }) //TODO may need to change find parameter (user._id)
       .populate('createdBy', '_id email')
       .then((fitness) => {
-          res.json(fitness);
+        res.json(fitness);
       })
       .catch((err) => {
-          console.log('ERROR', err);
-          res.status(400).json({ message: 'something went wrong in find all fitness goals', error: err });
+        res.status(400).json({ message: 'something went wrong in find all Fitness', error: err });
       })
-  })
-  .catch((err) => {
-      res.status(400).json({ message: 'something went wrong in find all Fitness', error: err });
-  });
+//       .catch((err) => {
+//         res.status(400).json({ message: 'something went wrong in find all Fitness', error: err });
+//       });
+// })
 }
 
 module.exports.createFitness = (req, res) => {
-    //TODO may needed when implementing JWT
-    const user = jwt.verify(req.cookies.userToken, SECRET);
-    Fitness.create({ ...req.body, createdBy: user._id })
-    // Fitness.create(req.body)
-        .then((newFitness) => {
-            res.status(201).json(newFitness)
-        })
-        .catch((err) => {
-            console.log('ERROR IN create Fitness', err);
-            res.status(400).json({message: 'Something went wrong', error:err})
-        });
+    // const user = jwt.verify(req.cookies.userToken, SECRET);
+  Fitness.create(req.body)
+    .then((newFitness) => {
+        res.status(201).json(newFitness)
+    })
+    .catch((err) => {
+        res.status(400).json({message: 'Something went wrong', error:err})
+    });
 }
 
 module.exports.updateFitness = (req, res) => {
-    Fitness.findOneAndUpdate({_id:req.params.id},
-        req.body,
-        {new:true, runValidators: true}
-        )
-        .then((updateFitness) => {
-            res.json(updateFitness)
-        })
-        .catch((err) => {
-            console.log('ERROR IN update Fitness', err);
-            res.status(400).json({message: 'Something went wrong', error:err})
-        });
+  Fitness.findOneAndUpdate({_id:req.params.id}, req.body, {new:true, runValidators: true})
+    .then((updateFitness) => {
+        res.json(updateFitness)
+    })
+    .catch((err) => {
+        res.status(400).json({message: 'Something went wrong', error:err})
+    });
 }
 
 module.exports.deleteFitness = (req,res) => {
--- server/controllers/mindfulness.controller.js
+++ server/controllers/mindfulness.controller.js
@@ -4,15 +4,12 @@ const SECRET = process.env.JWT_SECRET;
 const User = require('../models/user.model');
 
 
-
-module.exports.findAllMindfulnessGoals = (req, res) => {
-    Mindfulness.find({})
-        .populate('createdBy', 'email')
-        .then((allMindfulnessGoals) => {
-            res.json(allMindfulnessGoals)
+module.exports.findAllMindfulness = (req, res) => {
+    Mindfulness.find()
+        .then((allMindfulness) => {
+            res.json(allMindfulness)
         })
         .catch((err) => {
-            console.log('ERROR IN Get all', err);
             res.status(400).json({message: 'Something went wrong', error:err})
         });
 }
@@ -23,54 +20,46 @@ module.exports.findOneMindfulness = (req, res) => {
             res.json(mindfullness)
         })
         .catch((err) => {
-            console.log('ERROR IN Get Mindfulness', err);
             res.status(400).json({message: 'Something went wrong', error:err})
         });
 }
 
 module.exports.findMindfulnessByUser = (req, res) => {
-
-    console.log('IS THIS WORKING', req.params.email);
-    User.findOne({ email: req.params.email }).then((user) => {   //TODO decide whether to keep it until complete front end useState/localstorage
-        console.log('USERID', user._id);
-        Mindfulness.find({ createdBy: req.params.userId }) //TODO may need to change find parameter (user._id)
-        .populate('createdBy', '_id email')
-        .then((mindfulness) => {
-            res.json(mindfulness);
-        })
-        .catch((err) => {
-            console.log('ERROR', err);
-            res.status(400).json({ message: 'something went wrong in find all mindfulness goals', error: err });
-        })
-})
+// User.findOne({ username: req.params.username }).then((user) => {   //TODO decide whether to keep it until complete front end useState/localstorage
+//   console.log('USERID', user._id);
+  Mindfulness.find({ createdBy: req.params.userId }) //TODO may need to change find parameter (user._id)
+    .populate('createdBy', '_id email')
+    .then((mindfullness) => {
+      res.json(mindfullness);
+    })
+    .catch((err) => {
+      res.status(400).json({ message: 'something went wrong in find all mindfullness', error: err });
+    })
+//       .catch((err) => {
+//         res.status(400).json({ message: 'something went wrong in find all mindfullness', error: err });
+//       });
+// })
 }
 
 module.exports.createMindfulness = (req, res) => {
-    //TODO may needed when implementing JWT
-    const user = jwt.verify(req.cookies.userToken, SECRET);
-    Mindfulness.create({ ...req.body, createdBy: user._id })
-    // Mindfulness.create(req.body)
-        .then((newMindfulness) => {
-            res.status(201).json(newMindfulness)
-        })
-        .catch((err) => {
-            console.log('ERROR IN create Mindfulness', err);
-            res.status(400).json({message: 'Something went wrong', error:err})
-        });
+    // const user = jwt.verify(req.cookies.userToken, SECRET);
+  Mindfulness.create(req.body)
+    .then((newMindfulness) => {
+        res.status(201).json(newMindfulness)
+    })
+    .catch((err) => {
+        res.status(400).json({message: 'Something went wrong', error:err})
+    });
 }
 
 module.exports.updateMindfulness = (req, res) => {
-    Mindfulness.findOneAndUpdate({_id:req.params.id},
-        req.body,
-        {new:true, runValidators: true}
-        )
-        .then((updateMindfulness) => {
-            res.json(updateMindfulness)
-        })
-        .catch((err) => {
-            console.log('ERROR IN update Mindfulness', err);
-            res.status(400).json({message: 'Something went wrong', error:err})
-        });
+  Mindfulness.findOneAndUpdate({_id:req.params.id}, req.body, {new:true, runValidators: true})
+    .then((updateMindfulness) => {
+        res.json(updateMindfulness)
+    })
+    .catch((err) => {
+        res.status(400).json({message: 'Something went wrong', error:err})
+    });
 
 }
 
