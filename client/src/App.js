3155bc257ab6fee249940d593d75926fe07d6d7f
Hermanda Mak <hermandamak@gmail.com>
2022-08-19 16:11:16 -0700

    change login props

--- client/src/App.js
+++ client/src/App.js
@@ -15,20 +15,19 @@ import { useState } from 'react';
 function App() {
   
   const [loggedIn, setLoggedIn] = useState(false);
-  // const [userId, setUserId] = useState("");
   
   return (
     <div>
       <BrowserRouter>
         <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
         <Routes>
-          <Route path={'/'} element={<HomePage/>}></Route>
+          <Route path={'/'} element={<HomePage setLoggedIn={setLoggedIn} />} />
           <Route path={'/login'} element={<UserLogin setLoggedIn={setLoggedIn} />} />
           <Route path={'/register'} element={<UserRegistration setLoggedIn={setLoggedIn} />}/>
-          <Route path={'/about'} element={<About/>} />
-          <Route path={'/dashboard/'} element={<GoalDashboard />} />
-          <Route path={'/goal/add/:category'} element={<GoalAdd />} />
-          <Route path={'/goal/edit/:category/:id'} element={<GoalUpdate />} />
+          <Route path={'/about'} element={<About setLoggedIn={setLoggedIn} />} />
+          <Route path={'/dashboard/'} element={<GoalDashboard setLoggedIn={setLoggedIn} />} />
+          <Route path={'/goal/add/:category'} element={<GoalAdd setLoggedIn={setLoggedIn} />} />
+          <Route path={'/goal/edit/:category/:id'} element={<GoalUpdate setLoggedIn={setLoggedIn} />} />
         </Routes>
       </BrowserRouter>
     </div>
