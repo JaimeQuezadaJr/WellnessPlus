6b478fe2baf01fc35b5581ece952f8b9ff8ebf03
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2022-08-22 20:09:44 -0700

    margin top change

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -30,7 +30,7 @@ const HomePage = ({loggedIn, setLoggedIn}) => {
   return (
     <Container className='mt-5 mb-5'>
       <Card className='col-lg-6'>
-        <Card.Img variant="top" src="https://media.istockphoto.com/photos/wellness-sign-with-wooden-cubes-picture-id513319180?b=1&k=20&m=513319180&s=170667a&w=0&h=vhYdVc-IHZLyS3xqNVuUTcBFpsdJRKPmLzFQpgKnpJY=" />
+        <Card.Img className='img' variant="top" src="https://media.istockphoto.com/photos/wellness-sign-with-wooden-cubes-picture-id513319180?b=1&k=20&m=513319180&s=170667a&w=0&h=vhYdVc-IHZLyS3xqNVuUTcBFpsdJRKPmLzFQpgKnpJY=" />
         <Card.Body>
           <Card.Title>Track | Commit | Achieve</Card.Title>
           <Card.Text>
